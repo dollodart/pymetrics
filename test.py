56c1c46bce069ddd7ee2ce7aa078653ef9ff5d56
David Ollodart <davidollodart@gmail.com>
2022-03-13 14:04:42 -0500

    refactor tests into independent subroutines

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from counter import FastCounter, RecursiveCounter
 from auxcl import NodeDict
-from pathlib import Path
 import re
 import ast
+from pathlib import Path
+from radon.visitors import HalsteadVisitor # for comparison
+from time import time
+
 
 def table_report(mv):
+    print(f'### table for visitor type {mv.__class__}')
     print(f'count comparators={mv.comparators}')
     print(f'comprehensioness {mv.comprehensioness:.2f}')
     print(f'decoratedness {mv.statictypeness:.2f}')
@@ -18,6 +22,7 @@ def table_report(mv):
     print(f'average number of methods in class {mv.classsize:.1f}')
     print(f'average character length of a name {mv.nameslengths:.1f}')
     print(f'a-z character distribution {[round(x*100) for x in mv.namesalphdistr]}')
+    print('###')
 
 def plot_dist(nlines, nchars, nwords, nnodes):
     import matplotlib.pyplot as plt
@@ -75,59 +80,53 @@ def wc(rr):
         xy.append((nlines, nchars, nwords))
     return zip(*xy)
 
-
-from radon.visitors import HalsteadVisitor
-from time import time
-
-if __name__ == '__main__':
-    rr = dirstream('./ase/optimize')
-    rrm = [ast.parse(r) for r in rr]
-
+def test_fast_counter(list_parsed_source):
     # fc test
     mv = FastCounter()
-    mvs = []
     t0 = time()
-    for mod in rrm:
+    for mod in list_parsed_source:
         mv.generic_visit(mod)
+    print(f'FastCounter dt = {time() - t0:.3f} {len(list_parsed_source)} module test')
+
+    table_report(mv)
+
+def test_fast_counter_vs_wc(list_source_strings, list_parsed_source):
+    # run a distribution w.r.t. module
+    nlines, nchars, nwords = wc(list_source_strings)
+    mvs = []
+    for mod in list_parsed_source:
         mvs.append(FastCounter())
         mvs[-1].generic_visit(mod)
-    print(f'FastCounter dt = {time() - t0:.3f} {len(rrm)} module test')
-
-    print('-'*72)
-    table_report(mv)
-    print('-'*72)
-
-    t0 = time()
-    hv = HalsteadVisitor()
-    for mod in rrm:
-        hv.generic_visit(mod)
-    print(f'HalsteadVisitor dt = {time() - t0:.3f} {len(rrm)} module test')
-
-    # run a distribution w.r.t. module
-
-    nlines, nchars, nwords = wc(rr)
     plot_dist(nlines, nchars, nwords, [mv.n for mv in mvs])
 
+def test_halstead_visitor(list_parsed_source):
+    t0 = time()
+    hv = HalsteadVisitor()
+    for mod in list_parsed_source:
+        hv.generic_visit(mod)
+    print(f'HalsteadVisitor dt = {time() - t0:.3f} {len(list_parsed_source)} module test')
+
+def test_node_dict(list_parsed_source):
     # ndict test
     ndict = NodeDict()
     t0 = time()
-    for rr in rrm[:5]:
+    for mod in list_parsed_source:
         ndict.accumulate(mod)
-    print(f'NodeDict dt = {time() - t0:.3f} (5 module test)')
+    print(f'NodeDict dt = {time() - t0:.3f} ({len(list_parsed_source)} module test)')
     t0 = time()
-    print(f'NodeDict name distinctness = {ndict.namesdistinctness} dt = {time() - t0:.3f} (5 module test)') # slow computation
+    print(f'NodeDict name distinctness = {ndict.namesdistinctness} dt '
+    f'= {time() - t0:.3f} ({len(list_parsed_source)} module test)') # slow computation
 
-    # rc test
+def test_recursive_counter(list_parsed_source):
     mv = RecursiveCounter()
     t0 = time()
-    mv.generic_visit(ast.parse(rrm[len(rrm) // 3]))
+    for mod in list_parsed_source:
+        mv.generic_visit(mod)
 
     # this isn't the value in Recursive Counter
     mv.postwalk_merge()
-    print(f'RecursiveCounter dt = {time() - t0:.3f} (one module test)')
-    print('-'*72)
+    print(f'RecursiveCounter dt = {time() - t0:.3f} ({len(list_parsed_source)} module test)')
     table_report(mv)
-    print('-'*72)
 
     # a distribution of subtree statistics is of value (though it requires a similar merging procedure)
     l = []
@@ -135,5 +134,16 @@ if __name__ == '__main__':
         l.append(mv.n)
         for st in mv.subtrees:
             recur_n(st)
+    t0 = time()
     recur_n(mv)
-    print('distribution of number of ast nodes', sorted(l))
+    print(f'distribution of number of ast nodes {sorted(l)} dt = {time() - t0:.3f}')
+
+if __name__ == '__main__':
+    list_source_strings = dirstream('./ase/optimize')
+    list_parsed_source = [ast.parse(r) for r in list_source_strings]
+
+    test_fast_counter(list_parsed_source)
+    test_fast_counter_vs_wc(list_source_strings, list_parsed_source)
+    test_halstead_visitor(list_parsed_source)
+    test_node_dict(list_parsed_source[:5])
+    test_recursive_counter([list_parsed_source[len(list_parsed_source) // 3]])
